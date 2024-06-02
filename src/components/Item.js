756432efe67249902097950ca4779075c421c675
Sarah Azad <aras.azad3@gmail.com>
2024-06-03 00:39:22 +0300

    done

---
+++ src/components/Item.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  return (
+    <li className={inCart? 'in-cart' : ''}>
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button className="add" onClick={()=> setInCart((inCart) => !inCart)}>Add to Cart</button>
+    </li>
+  );
+}
+
+export default Item;
